4d7a52131736c2f59d935e738724fa4feae8b21a
Rlynette <lynette.ronnoh@student.moringaschool.com>
2022-08-22 14:08:12 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 // Write your code here!
-document.querySelector(`#main`).remove();
-var newHeader = document.querySelector(`h1#victory`);
+document.querySelector('#main').remove();
+var newHeader = document.querySelector('h1#victory');
